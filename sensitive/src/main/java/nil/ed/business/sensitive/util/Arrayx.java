26bb76ceda6a27c98a4577ef31f6a360d62d15c3
delin10 <lidelin10@outlook.com>
2019-10-29 15:54:36 +0800

    add/添加返回值为StringBuilder的repeat方法

--- sensitive/src/main/java/nil/ed/business/sensitive/util/Arrayx.java
+++ sensitive/src/main/java/nil/ed/business/sensitive/util/Arrayx.java
@@ -11,4 +11,10 @@ public class Arrayx {
     public static String repeat(String target, int count){
         return IntStream.range(0, count).mapToObj(i -> target).collect(Collectors.joining());
     }
+
+    public static StringBuilder repeatToStringBuilder(String target, int count){
+        StringBuilder builder = new StringBuilder();
+        IntStream.range(0, count).mapToObj(String::valueOf).forEach(builder::append);
+        return builder;
+    }
 }
